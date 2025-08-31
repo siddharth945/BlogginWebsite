26a10ccc4caaeb607ee56460a1d62ddcaebfd39a
Siddharth Kumar Chouhan <you@example.com>
2025-08-31 17:48:08 +0530

    Fix Vercel deployment: export app, add api/index.js, update vercel.json

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const { checkForAuthenticationCookie } = require('./middlewares/authentication');
 const blogRoute = require('./routes/blog');
 const app = express();
-const PORT = process.env.PORT ||  8001;
+/* const PORT = process.env.PORT ||  8001; */
 const Blog = require('./models/blogs.model');
 
 app.set('view engine','ejs');
@@ -45,6 +45,10 @@ app.use('/user',userrouter);
 app.use('/blog',blogRoute);
 
 
-app.listen(PORT,()=>{
+/* app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
-});
+}); */
+
+app.use(express.json());
+
+module.exports = app;
